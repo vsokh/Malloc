1f004b56beb2510862d270b623d89dd566b7be30
vsokh <vsokolohorskyi@gmail.com>
2021-03-25 16:37:19 +0200

    Align funcs' prototypes

--- src/utils.c
+++ src/utils.c
@@ -6,33 +6,38 @@
 /*   By: vsokolog <vsokolog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 18:17:21 by vsokolog          #+#    #+#             */
-/*   Updated: 2021/03/25 13:02:52 by vsokolog         ###   ########.fr       */
+/*   Updated: 2021/03/25 15:36:40 by vsokolog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
-size_t	align4(size_t x)
+static void	print_block(t_block *p)
+{
+	printf("[%11p<-(%p)->%11p]: %c, S:%zu\n",
+			p->prev, p, p->next, !p->inuse ? 'F' : 'A', p->size);
+}
+
+size_t		align4(size_t x)
 {
 	return ((((x - 1) >> 2) << 2) + 4);
 }
 
-size_t	align_to_page(size_t x)
+size_t		align_to_page(size_t x)
 {
 	return (getpagesize() * (x / getpagesize() + 1));
 }
 
-void	print_block(t_block *p)
+void		show_mem(void)
 {
-	printf("[%11p<-(%p)->%11p]: %c, S:%zu\n", p->prev, p, p->next, !p->inuse ? 'F':'A', p->size);
-}
+	t_block	*p;
+	int		i;
 
-void	show_mem(void)
-{
-	t_block *p = g_first;
-	int i = 0;
+	i = 0;
+	p = g_first;
 	printf("====================================================\n");
-	if (p==NULL) { printf("EMPTY\n"); }
+	if (p == NULL)
+		printf("EMPTY\n");
 	while (p != NULL)
 	{
 		print_block(p);
@@ -41,4 +46,3 @@ void	show_mem(void)
 	}
 	printf("====================================================\n");
 }
-
