242e127c134d273dffe48bbaad2c8353d000fb39
vsokh <vsokolohorskyi@gmail.com>
2021-04-12 11:25:44 +0300

    Update header

--- inc/malloc.h
+++ inc/malloc.h
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   malloc.h                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vsokolog <vsokolog@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/16 15:55:41 by vsokolog          #+#    #+#             */
-/*   Updated: 2021/04/07 16:06:55 by vsokolog         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #ifndef MALLOC_H
 # define MALLOC_H
@@ -25,14 +14,70 @@
 
 # include <errno.h>
 
-/* For size_t */
-# include <stddef.h>
+/* # define TINY_PAGES		(1 << 2) /1*  4 pages *1/ */
+/* # define SMALL_PAGES		(1 << 5) /1* 32 pages *1/ */
 
-/* For uintptr_t.  */
-# include <stdint.h>
+# define MIN_SIZE			(2 * sizeof(size_t))
+# define PAGE_SIZE 			(getpagesize())
 
-# include <stdlib.h>
+# define AVAIL 				0x1
+# define PARTIAL			0x2
+# define FULL 				0x4
+
+# define TINY_SLAB_SIZE		(1 << 7)  /*  128 bytes  */
+# define SMALL_SLAB_SIZE	(1 << 10) /* 1024 bytes  */
+# define SLABS				(1 << 7)  /*  128        */
+
+# define TINY_ZONE_SIZE		(SLABS * TINY_SLAB_SIZE)
+# define SMALL_ZONE_SIZE	(SLABS * SMALL_SLAB_SIZE)
 
 # define align(x, alignment)	\
-	((x) + (alignment) - 1) & ~((alignment) - 1)
+	(((x) + (alignment) - 1) & ~((alignment) - 1))
 
+# define align2page(x)			\
+	(align(x, PAGE_SIZE))
+
+# define align2minsize(x)		\
+	(align(x, MIN_SIZE))
+
+# define size2blocksz(x)		\
+	(align2minsize(x) + sizeof(struct s_block))
+
+# define block2mem(block)		\
+	((void*)((char*)block + sizeof(struct s_block)))
+
+# define mem2block(mem)			\
+	((void*)((char*)mem - sizeof(struct s_block)))
+
+# define datasize(block)		\
+	(((block)->size) - sizeof(struct s_block))
+
+# define isinuse(block)			\
+	((block)->inuse & AVAIL)
+
+typedef struct		s_block
+{
+	struct s_block	*prev;
+	struct s_block	*next;
+	size_t			inuse;
+	size_t			size;
+}					t_block;
+
+typedef struct		s_slab
+{
+	struct s_block	*blocks;
+	/* size_t			flags; /1* Avail, partial, full *1/ */
+}					t_slab;
+
+typedef struct		s_arena
+{
+	struct s_slab	tiny_zone[SLABS];
+	struct s_slab	small_zone[SLABS];
+	struct s_block	*large_zone;
+}					t_arena;
+
+extern t_arena		g_arena;
+void				*malloc(size_t size);
+void				free(void *ptr);
+
+void				show_mem(void);
