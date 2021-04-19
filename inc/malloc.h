76bc2ddfab8c7aebbc44d9eaac6a03628d294dd8
vsokh <vsokolohorskyi@gmail.com>
2021-04-19 15:59:57 +0300

    Update header

--- inc/malloc.h
+++ inc/malloc.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsokolog <vsokolog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 15:27:30 by vsokolog          #+#    #+#             */
+/*   Updated: 2021/04/19 15:54:09 by vsokolog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MALLOC_H
 # define MALLOC_H
@@ -14,72 +25,94 @@
 
 # include <errno.h>
 
-/* # define TINY_PAGES		(1 << 2) /1*  4 pages *1/ */
-/* # define SMALL_PAGES		(1 << 5) /1* 32 pages *1/ */
+# include "libft.h"
 
-# define MIN_SIZE			(2 * sizeof(size_t))
-# define PAGE_SIZE 			(getpagesize())
+# include "pthread.h"
 
-# define AVAIL 				0x1
-# define PARTIAL			0x2
-# define FULL 				0x4
+# define MIN_SIZE				(2 * sizeof(size_t))
+# define PAGE_SIZE 				(getpagesize())
 
-# define TINY_SLAB_SIZE		(1 << 7)  /*  128 bytes  */
-# define SMALL_SLAB_SIZE	(1 << 10) /* 1024 bytes  */
-# define SLABS				(1 << 7)  /*  128        */
+# define TINY_DATA_SIZE			128
+# define SMALL_DATA_SIZE		1024
+# define ALLOCATIONS			128
 
-# define TINY_ZONE_SIZE		(SLABS * TINY_SLAB_SIZE)
-# define SMALL_ZONE_SIZE	(SLABS * SMALL_SLAB_SIZE)
+# define BLOCK_METADATA_SIZE	sizeof(t_meta_data)
+# define ZONE_METADATA_SIZE		sizeof(t_zone)
 
-# define align(x, alignment)	\
+# define TINY_ZONE_SIZE			(ZONE_METADATA_SIZE + TINY_DATA_SIZE  * ALLOCATIONS)
+# define SMALL_ZONE_SIZE		(ZONE_METADATA_SIZE + SMALL_DATA_SIZE * ALLOCATIONS)
+
+# define TINY_BLOCK_SIZE		(BLOCK_METADATA_SIZE + TINY_DATA_SIZE)
+# define SMALL_BLOCK_SIZE		(BLOCK_METADATA_SIZE + SMALL_DATA_SIZE)
+
+# define TINY_BLOCKS_NUM		((TINY_ZONE_SIZE  - ZONE_METADATA_SIZE) / TINY_BLOCK_SIZE)
+# define SMALL_BLOCKS_NUM		((SMALL_ZONE_SIZE - ZONE_METADATA_SIZE) / SMALL_BLOCK_SIZE)
+
+# define align(x, alignment)		\
 	(((x) + (alignment) - 1) & ~((alignment) - 1))
 
-# define align2page(x)			\
-	(align(x, PAGE_SIZE))
+# define align2page(x)				\
+	((x) ? (align(x, PAGE_SIZE)) : PAGE_SIZE)
 
-# define align2minsize(x)		\
-	(align(x, MIN_SIZE))
+# define align2minsize(x)			\
+	((x) ? (align(x, MIN_SIZE)) : MIN_SIZE)
 
-# define size2blocksz(x)		\
-	(align2minsize(x) + sizeof(struct s_block))
+# define req2size(x)				\
+	(align2minsize(x) + BLOCK_METADATA_SIZE)
 
-# define block2mem(block)		\
-	((void*)((char*)block + sizeof(struct s_block)))
+# define zone2mem(zone)				\
+	((zone) ? ((void*)((char*)(zone) + ZONE_METADATA_SIZE)) : NULL)
 
-# define mem2block(mem)			\
-	((void*)((char*)mem - sizeof(struct s_block)))
+# define block_head_at(zone, idx)	\
+	((zone) ? ((zone2mem(zone) + (zone)->block_size * idx)) : NULL)
 
-# define datasize(block)		\
-	(((block)->size) - sizeof(struct s_block))
+# define block2mem(block)			\
+	((block) ? ((void*)((char*)(block) + BLOCK_METADATA_SIZE)) : NULL)
 
-# define isinuse(block)			\
-	((block)->inuse & AVAIL)
+# define mem2block(mem)				\
+	((mem) ? ((void*)((char*)(mem) - BLOCK_METADATA_SIZE)) : NULL)
 
-typedef struct		s_block
+# define datasize(block)			\
+	((block) ? ((block)->size - BLOCK_METADATA_SIZE) : 0)
+
+typedef struct			s_meta_data
 {
-	struct s_block	*prev;
-	struct s_block	*next;
-	size_t			inuse;
-	size_t			size;
-}					t_block;
+	struct s_meta_data	*prev;
+	struct s_meta_data	*next;
 
-typedef struct		s_slab
+	size_t				inuse;
+	size_t				size;
+}						t_meta_data;
+
+typedef struct			s_zone
 {
-	struct s_block	*blocks;
-	/* size_t			flags; /1* Avail, partial, full *1/ */
-}					t_slab;
+	size_t				block_size;
+	size_t				block_num;
+}						t_zone;
 
-typedef struct		s_arena
+typedef struct			s_arena
 {
-	struct s_slab	tiny_zone[SLABS];
-	struct s_slab	small_zone[SLABS];
-	struct s_block	*large_zone;
-}					t_arena;
+	t_zone 				*tiny_zone;
+	t_zone 				*small_zone;
+	t_meta_data			*large_zone;
+}						t_arena;
 
-extern t_arena		g_arena;
-void				*malloc(size_t size);
-void				free(void *ptr);
+extern t_arena			g_arena;
+extern pthread_mutex_t	g_mtx;
 
-void				show_alloc_mem(void);
+void					free(void *ptr);
+void					*malloc(size_t size);
+void					*realloc(void *ptr, size_t size);
+
+int						alloc_mem(void **mem, size_t size);
+
+int						try_alloc_tinysmall_block(void **mem, size_t size);
+int						try_free_tinysmall_block(void **ptr);
+
+int						try_alloc_large_block(void **mem, size_t size);
+int						try_free_large_block(void **ptr);
+
+void					show_alloc_mem(void);
+void					print_block(t_meta_data *block);
 
 #endif
